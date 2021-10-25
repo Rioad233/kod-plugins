e27d65b381df31f0f8142115eaddd72b865b2df5
fansheng <185085781@qq.com>
2021-10-25 13:34:03 +0800

    1

--- aliyunDrive/lib/getVersion.php
+++ aliyunDrive/lib/getVersion.php
@@ -1,2 +1,2 @@
 <?php
-echo file_get_contents("");
+echo file_get_contents("https://gitee.com/fs185085781/kod-plugins/raw/master/aliyunDrive/package.json");
--- aliyunDrive/static/update.js
+++ aliyunDrive/static/update.js
@@ -1,4 +1,49 @@
 (function (){
+    function getCurrentScript() {
+        var js = "update.js";
+        var script = document.currentScript;
+        if(!script && document.querySelector){
+            script = document.querySelector("script[src*='"+js+"']");
+        }
+        if(!script){
+            var scripts = document.getElementsByTagName("script");
+            for (var i = 0, l = scripts.length; i < l; i++) {
+                var src = scripts[i].src;
+                if (src.indexOf(js) != -1) {
+                    script = scripts[i];
+                    break;
+                }
+            }
+        }
+        return script;
+    }
+    function getHost(src,length){
+        var ss = src.split("/");
+        ss.length = ss.length - length;
+        var path = ss.join("/");
+        return path;
+    }
+    function asyncLoadData(url,fn){
+        var xmlhttp;
+        if (window.XMLHttpRequest){
+            xmlhttp=new XMLHttpRequest();
+        }else{
+            xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        xmlhttp.onreadystatechange=function(){
+            if(xmlhttp.readyState==4){
+                try{
+                    fn(xmlhttp.responseText);
+                }catch (e) {
+                    console.warn(e);
+                }
+            }
+        }
+        xmlhttp.open("GET",url,true);
+        xmlhttp.send();
+    }
+    var script = getCurrentScript();
+    var host = getHost(script.src,2);
     var ready = function (callback){
         if(window.$ && document.readyState == "complete"){
             callback();
@@ -7,13 +52,15 @@
         }
     }
     ready(function (){
-        var flag = confirm("检测到gitee有更新的版本,是否跳转?");
-        if(!flag){
-            var flag2 = confirm("下次是否继续提醒?");
-            if(flag2){
-
-            }
-            return;
-        }
+        asyncLoadData(host+"/package.json",function(localJsonStr){
+            var localVersion = JSON.parse(localJsonStr).version;
+            asyncLoadData(host+"/lib/getVersion.php",function(remoteJsonStr){
+                var remoteVersion = JSON.parse(remoteJsonStr).version;
+                console.log("version",localVersion,remoteVersion);
+                if(localVersion == remoteVersion){
+                    return;
+                }
+            });
+        });
     });
 })()
